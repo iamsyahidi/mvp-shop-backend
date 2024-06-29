8b080507def138a79e3c1374df880f1018af1056
iamsyahidi <ilhamsyahidi66@gmail.com>
2024-06-29 15:41:43 +0700

    feat: add mw responses

---
+++ middleware/response.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"encoding/json"
+	"mvp-shop-backend/models"
+	"mvp-shop-backend/pkg/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Response setting gin.JSON
+func Response(c *gin.Context, req interface{}, res models.Response) {
+	// LOGGER
+	reqByte, _ := json.Marshal(req)
+	resByte, _ := json.Marshal(res)
+	logger.Infof("[dbo-is-backend:log] [RequestURL] : %s, [RequestMethod] : %s, [RequestBody] : %s, [ResponseData] : %s", c.Request.RequestURI, c.Request.Method, string(reqByte), string(resByte))
+
+	c.JSON(res.Code, res)
+}
