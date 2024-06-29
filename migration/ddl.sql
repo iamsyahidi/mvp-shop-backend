1d6f667fc0914db02cc77d656da3b24c7d91a522
iamsyahidi <ilhamsyahidi66@gmail.com>
2024-06-29 18:43:08 +0700

    feat: add ddl table product

--- migration/ddl.sql
+++ migration/ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.customer (
+CREATE TABLE IF NOT EXISTS public.customers (
 	id varchar(36) NOT NULL,
 	email varchar(100) NOT NULL,
 	"name" varchar(250) NOT NULL,
@@ -8,12 +8,45 @@ CREATE TABLE IF NOT EXISTS public.customer (
 	created_by varchar(150) NOT NULL,
 	updated_at timestamptz NULL,
 	updated_by varchar(150) DEFAULT NULL::character varying NULL,
-	CONSTRAINT customer_pkey PRIMARY KEY (id)
+	CONSTRAINT customers_pkey PRIMARY KEY (id)
 );
-CREATE INDEX IF NOT EXISTS idx_customer_email ON public.customer USING btree (email);
-CREATE INDEX IF NOT EXISTS idx_customer_id ON public.customer USING btree (id);
-CREATE INDEX IF NOT EXISTS idx_customer_name ON public.customer USING btree ("name");
-CREATE INDEX IF NOT EXISTS idx_customer_password ON public.customer USING btree ("password");
-CREATE INDEX IF NOT EXISTS idx_customer_status ON public.customer USING btree ("status");
+CREATE INDEX IF NOT EXISTS idx_customers_email ON public.customers USING btree (email);
+CREATE INDEX IF NOT EXISTS idx_customers_id ON public.customers USING btree (id);
+CREATE INDEX IF NOT EXISTS idx_customers_name ON public.customers USING btree ("name");
+CREATE INDEX IF NOT EXISTS idx_customers_password ON public.customers USING btree ("password");
+CREATE INDEX IF NOT EXISTS idx_customers_status ON public.customers USING btree ("status");
 
-INSERT INTO public.customer (id, email, "name", "password", "status", created_at, created_by, updated_at, updated_by) VALUES(gen_random_uuid(), 'ilhamsyahidi66@gmail.com', 'ilham', '$2a$10$2RLJwYzS2wCGTkgr145miOvXe52nI0JRE.8uOgqEmM8ulmmRufWle', 'active', now(), 'ilham', NULL, NULL);
+INSERT INTO public.customers (id, email, "name", "password", "status", created_at, created_by, updated_at, updated_by) VALUES(gen_random_uuid(), 'ilhamsyahidi66@gmail.com', 'ilham', '$2a$10$2RLJwYzS2wCGTkgr145miOvXe52nI0JRE.8uOgqEmM8ulmmRufWle', 'active', now(), 'ilham', NULL, NULL);
+
+CREATE TABLE IF NOT EXISTS public.product_categories (
+	id varchar(36) NOT NULL,
+	"name" varchar(250) NOT NULL,
+	"status" varchar(10) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	created_by varchar(150) NOT NULL,
+	updated_at timestamptz NULL,
+	updated_by varchar(150) DEFAULT NULL::character varying NULL,
+	CONSTRAINT product_categories_pkey PRIMARY KEY (id)
+);
+CREATE INDEX IF NOT EXISTS idx_product_categories_id ON public.product_categories USING btree (id);
+CREATE INDEX IF NOT EXISTS idx_product_categories_name ON public.product_categories USING btree ("name");
+CREATE INDEX IF NOT EXISTS idx_product_categories_status ON public.product_categories USING btree ("status");
+
+CREATE TABLE IF NOT EXISTS public.products (
+	id varchar(36) NOT NULL,
+	"name" varchar(250) NOT NULL,
+	price numeric NULL,
+	stock numeric NULL,
+	"status" varchar(10) NOT NULL,
+	"category_id" varchar(36) NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	created_by varchar(150) NOT NULL,
+	updated_at timestamptz NULL,
+	updated_by varchar(150) DEFAULT NULL::character varying NULL,
+	CONSTRAINT products_pkey PRIMARY KEY (id)
+);
+CREATE INDEX IF NOT EXISTS idx_products_id ON public.products USING btree (id);
+CREATE INDEX IF NOT EXISTS idx_products_name ON public.products USING btree ("name");
+CREATE INDEX IF NOT EXISTS idx_products_price ON public.products USING btree (price);
+CREATE INDEX IF NOT EXISTS idx_products_stock ON public.products USING btree (stock);
+CREATE INDEX IF NOT EXISTS idx_products_status ON public.products USING btree ("status");
